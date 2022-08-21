ed87fee5f8ef3063e0c958338d7efc6accf1ad27
igorpex <igor.bogdanov@gmail.com>
2022-08-22 00:02:21 +0300

    feat: add interfaces for REST client module (dto)

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,6 +1,58 @@
-interface IRoute {
+export interface IRoute {
   name: string;
   component: () => void;
 }
 
-export default IRoute;
+export interface User {
+  name?: string;
+  email: string;
+  password: string;
+}
+
+export interface UpdateUser {
+  name?: string;
+  email?: string;
+  password?: string;
+}
+
+export interface Word {
+  id: string,
+  group: 0,
+  page: 0,
+  word: string,
+  image: string,
+  audio: string,
+  audioMeaning: string,
+  audioExample: string,
+  textMeaning: string,
+  textExample: string,
+  transcription: string,
+  wordTranslate: string,
+  textMeaningTranslate: string,
+  textExampleTranslate: string
+}
+
+export type WordId = string;
+export interface UserWord {
+  difficulty: string;
+  optional: {}
+}
+
+export interface UserAggregatedWordsParams {
+  id: string;
+  group: string;
+  page: number;
+  wordsPerPage: number;
+  filter: Object;
+  token: string
+}
+
+export interface UserStatistics {
+  learnedWords: number;
+  optional: {}
+}
+
+export interface UserSettings {
+  wordsPerDay: number;
+  optional: {}
+}
