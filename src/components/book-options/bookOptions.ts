618bb9a95d01740cb693fe35be48fea18c2273bc
alpekhota <alekspekhota@gmail.com>
2022-08-24 18:58:03 +0200

    feat: add optionButtons to bookOptions

--- src/components/book-options/bookOptions.ts
+++ src/components/book-options/bookOptions.ts
@@ -1,6 +1,9 @@
 import Component from "../../utils/component";
 import Pagination from "../shared/pagination/pagination";
+import UIButton from "../UI/button/button";
 import BookInput from "./bookInput";
+import OptionButtons from "./optionButtons";
+import './options.scss';
 
 class BookOptions extends Component{
     updatePage: (page: number) => void = () => {};
@@ -8,6 +11,7 @@ class BookOptions extends Component{
     
     private input: BookInput;
     private pagination: Pagination;
+    private optionButtons: OptionButtons;
 
     group = 0;
 
@@ -19,6 +23,7 @@ class BookOptions extends Component{
         super(parentNode, 'div', ['book-options']);
         this.input = new BookInput(this.element, 'text', ['book-input']);
         this.pagination = new Pagination(this.element);
+        this.optionButtons = new OptionButtons(this.element);
 
         this.pagination.updatePage = (page) => this.updatePage(page);
         this.input.updateGroup = (group) => this.updateGroup(group);
