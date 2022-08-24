92fd5d0f1d1c43b2ca70cebaa720f215029757af
alpekhota <alekspekhota@gmail.com>
2022-08-24 15:38:00 +0200

    feat: add function getCards to Book

--- src/pages/book/book.ts
+++ src/pages/book/book.ts
@@ -1,14 +1,43 @@
+import { getAllWords, getWords } from "../../api/api";
 import BookContainer from "../../components/book-container/bookContainer";
+import { Word } from "../../interfaces";
 import Component from "../../utils/component";
 import './book.scss';
 
 class Book extends Component{
     private bookContainer: BookContainer;
+    page = 0;
+    group = 0;
 
     constructor(parentNode: HTMLElement) {
         super(parentNode, 'div', ['book']);
+        this.getCards(this.group, this.page);
 
         this.bookContainer = new BookContainer(this.element);
+        this.bookContainer.updatePage = (page) => {
+            this.page = page;
+            this.getCards(this.group - 1, this.page);
+        }
+        this.bookContainer.updateGroup = (group) => {
+            this.group = group;
+            console.log(this.group);
+            this.getCards(this.group - 1, this.page);
+        }
+    }
+
+    private async getCards(group: number, page: number) {
+        const data = await getWords({group, page});
+        if (data) {
+            const cardsArr: Word[] = data.items;
+            this.bookContainer.addWords(cardsArr);
+
+            // this.bookContainer.pagination.updateNextButton(
+            //     this.page,
+            //     3600,
+            //     20,
+            // );
+        }
+        
     }
 }
 
