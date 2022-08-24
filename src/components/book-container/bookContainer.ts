0cd0f4492bcc9c8df88b55b1107219734cecbc67
alpekhota <alekspekhota@gmail.com>
2022-08-24 16:10:51 +0200

    feat:  remove pagination from BookContainer

--- src/components/book-container/bookContainer.ts
+++ src/components/book-container/bookContainer.ts
@@ -1,8 +1,6 @@
-import { getWords } from "../../api/api";
 import { Word } from "../../interfaces";
 import Component from "../../utils/component";
 import BookOptions from "../book-options/bookOptions";
-import Pagination from "../shared/pagination/pagination";
 import './bookContainer.scss'
 import BookItem from "./bookItem";
 
@@ -13,30 +11,19 @@ class BookContainer extends Component{
     private title: Component;
     private mainContent: Component;
     private bookOptions: BookOptions;
-    pagination: Pagination;
     private cards: BookItem[];
 
     constructor(parentNode: HTMLElement) {
         super(parentNode, 'div', ['book-container']);
-        // this.drawWords(this.group, this.page);
         this.cards = [];
 
         this.title = new Component(this.element, 'h2', ['book__title'], 'Book');
         this.bookOptions = new BookOptions(this.element, 'div', ['book__setting']);
-        this.pagination  = new Pagination(this.element);
         this.mainContent = new Component(this.element, 'div', ['book__content']);
 
-        this.pagination.updatePage = (page) => this.updatePage(page);
+        this.bookOptions.updatePage = (page) => this.updatePage(page);
         this.bookOptions.updateGroup = (group) => this.updateGroup(group);
     }
-    // async drawWords(group: number, page: number){
-    //     const cards = await getWords({group, page});
-    //     this.cards = cards.items.map((card: Word) => {
-    //         const item = new BookItem(this.mainContent.element, card);
-    //         return item;
-    //     })
-
-    // }
 
     private clear() {
         this.mainContent.element.innerHTML = '';
