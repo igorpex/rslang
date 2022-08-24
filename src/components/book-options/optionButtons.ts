b183a56b7130cab716eb5ea3dc1b70272ce2aa2b
alpekhota <alekspekhota@gmail.com>
2022-08-24 20:32:40 +0200

    feat: add event on optionButtons click

--- src/components/book-options/optionButtons.ts
+++ src/components/book-options/optionButtons.ts
@@ -1,3 +1,4 @@
+import { Word } from "../../interfaces";
 import Component from "../../utils/component";
 import UIButton from "../UI/button/button";
 
@@ -8,12 +9,23 @@ class OptionButtons extends Component {
     
     constructor(parentNode: HTMLElement){
         super(parentNode, 'div', ['boook-options__buttons']);
+
         const sprintBtn = new UIButton(this.element, ['option-button', 'sprint-button'], '');
         sprintBtn.element.style.backgroundImage = `url(./sprint2.svg)`;
+
         const audioGameBtn = new UIButton(this.element, ['option-button', 'audio-game-button'], '');
         audioGameBtn.element.style.backgroundImage = `url(./audio-game2.svg)`;
+
         const dictionaryBtn = new UIButton(this.element, ['option-button', 'dictionary-button'], '');
         dictionaryBtn.element.style.backgroundImage = `url(./dictionary2.svg)`;
+
+        sprintBtn.onClickButton = () => {
+            this.startSprintGame();
+            console.log('start');
+        }
+        audioGameBtn.onClickButton = () => this.startAudioGame();
+        dictionaryBtn.onClickButton = () => this.openDictionary();
+
     }
 }
 
