fe3d97a3dca919dbb2552098eaf749cc04d6756f
alpekhota <alekspekhota@gmail.com>
2022-08-24 15:36:26 +0200

    feat: add pagination to bookContainer

--- src/components/book-container/bookContainer.ts
+++ src/components/book-container/bookContainer.ts
@@ -1,35 +1,55 @@
 import { getWords } from "../../api/api";
 import { Word } from "../../interfaces";
 import Component from "../../utils/component";
+import BookOptions from "../book-options/bookOptions";
+import Pagination from "../shared/pagination/pagination";
 import './bookContainer.scss'
 import BookItem from "./bookItem";
 
 class BookContainer extends Component{
+    updatePage: (page: number) => void = () => {};
+    updateGroup: (group: number) => void = () => {};
+
     private title: Component;
     private mainContent: Component;
-    private settingsPanel: Component;
-    private cards: Word[];
+    private bookOptions: BookOptions;
+    pagination: Pagination;
+    private cards: BookItem[];
 
     constructor(parentNode: HTMLElement) {
         super(parentNode, 'div', ['book-container']);
-        this.drawWords(0, 1);
+        // this.drawWords(this.group, this.page);
         this.cards = [];
-        this.title = new Component(this.element, 'h2', ['book__title']);
-        this.title.element.innerHTML = 'Book';
-        this.settingsPanel = new Component (this.element, 'div', ['book__setting'],'Setting for book');
+
+        this.title = new Component(this.element, 'h2', ['book__title'], 'Book');
+        this.bookOptions = new BookOptions(this.element, 'div', ['book__setting']);
+        this.pagination  = new Pagination(this.element);
         this.mainContent = new Component(this.element, 'div', ['book__content']);
+
+        this.pagination.updatePage = (page) => this.updatePage(page);
+        this.bookOptions.updateGroup = (group) => this.updateGroup(group);
     }
-    async drawWords(group: number, page: number){
-        const cards = await getWords({group, page});
-        this.cards = cards.items.map((card: Word) => {
+    // async drawWords(group: number, page: number){
+    //     const cards = await getWords({group, page});
+    //     this.cards = cards.items.map((card: Word) => {
+    //         const item = new BookItem(this.mainContent.element, card);
+    //         return item;
+    //     })
+
+    // }
+
+    private clear() {
+        this.mainContent.element.innerHTML = '';
+        this.cards = [];
+    }
+
+    addWords(cards: Word[]): void {
+        this.clear();
+
+        this.cards = cards.map((card: Word) => {
             const item = new BookItem(this.mainContent.element, card);
             return item;
         })
-
-    }
-
-    private clear() {
-
     }
 }
 
