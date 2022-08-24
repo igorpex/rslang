1e512715c53d1ceb7db3379981de641cec540e4b
alpekhota <alekspekhota@gmail.com>
2022-08-24 16:10:19 +0200

    feat: add pagination to BookOptions

--- src/components/book-options/bookOptions.ts
+++ src/components/book-options/bookOptions.ts
@@ -1,19 +1,26 @@
 import Component from "../../utils/component";
-import UIInput from "../UI/input/input";
+import Pagination from "../shared/pagination/pagination";
 import BookInput from "./bookInput";
 
 class BookOptions extends Component{
+    updatePage: (page: number) => void = () => {};
     updateGroup: (group: number) => void = () => {};
+    
+    private input: BookInput;
+    private pagination: Pagination;
+
     group = 0;
-    input: BookInput;
 
     constructor(
         parentNode: HTMLElement,
         buttonText: string,
         styles: string[] = [],
     ) {
-        super(parentNode, 'div', ['wrapper-input']);
+        super(parentNode, 'div', ['book-options']);
         this.input = new BookInput(this.element, 'text', ['book-input']);
+        this.pagination = new Pagination(this.element);
+
+        this.pagination.updatePage = (page) => this.updatePage(page);
         this.input.updateGroup = (group) => this.updateGroup(group);
     }
 }
