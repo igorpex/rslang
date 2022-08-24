3861b660d54a2934fdf5ce6f1ac49d86ff03cbd6
alpekhota <alekspekhota@gmail.com>
2022-08-24 15:34:48 +0200

    feat: create BookInput

---
+++ src/components/book-options/bookInput.ts
@@ -0,0 +1,29 @@
+import Component from "../../utils/component";
+import UIInput from "../UI/input/input";
+
+class BookInput extends Component{
+    input: UIInput;
+    updateGroup: (group: number) => void =() => {};
+    private group = 0;
+
+    constructor(
+        parentNode: HTMLElement,
+        buttonText: string,
+        styles: string[] = [],
+    ) {
+        super(parentNode, 'div', ['wrapper-input']);
+        this.input = new UIInput(this.element, 'select', 'text', ['book-input']);
+        const select = this.input.element as HTMLInputElement;
+
+        for( let i = 1; i <= 6; i++) {
+          const option = new Component(select, 'option', ['ui-input__option'], `Section ${i}`);
+          const item = option.element as HTMLInputElement;
+          item.value = `${i}`;
+        }
+        select.addEventListener('change', () => {
+            this.updateGroup(Number(select.value));
+            console.log(select.value);
+          });
+    }
+} 
+export default BookInput;
