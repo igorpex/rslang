55d761e047fedd54d03967958f63aa9979059f55
alpekhota <alekspekhota@gmail.com>
2022-08-25 11:47:51 +0200

    feat: add teamItem to TeamContainer

--- src/components/team-container/team-container.ts
+++ src/components/team-container/team-container.ts
@@ -1,6 +1,9 @@
+import { ITeam } from '../../interfaces';
 import Component from '../../utils/component';
 
 import './index.scss';
+import TeamItem from './teamItem';
+import team from './teamlist';
 
 class TeamContainer extends Component {
   // private container: Component;
@@ -8,14 +11,26 @@ class TeamContainer extends Component {
   private title: Component;
 
   private content: Component;
+  private aboutProject: Component;
+  team: ITeam[];
 
   constructor(parentNode: HTMLElement) {
     super(parentNode, 'div', ['team-container']);
+    this.team = team;
 
-    this.title = new Component(this.element, 'h2');
-    this.title.element.innerHTML = 'This is a team page';
     // this.container = new Component(this.element, 'div', ['team-container']);
+    this.aboutProject = new Component(this.element, 'div', ['project-content']);
+    const projectTitle = new Component(this.aboutProject.element, 'h2', ['project__title'], 'About our project');
+    const projectDiscription =  new Component(this.aboutProject.element, 'p', ['project__discription'], '');
+    projectDiscription.element.innerHTML = "This is.................................."
     this.content = new Component(this.element, 'div', ['team-content']);
+    this.title = new Component(this.content.element, 'h2', ['team__title']);
+    this.title.element.innerHTML = 'Our team';
+    const contentItems = new Component(this.content.element, 'div', ['team__list']);
+
+    this.team.forEach((member) => {
+      const item = new TeamItem(contentItems.element, member);
+    })
   }
 
   private clear() {
