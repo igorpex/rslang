093be6c14cd16589e88ae80f1e87fc630c674f6c
alpekhota <alekspekhota@gmail.com>
2022-08-25 11:46:00 +0200

    feat: create object with team info

---
+++ src/components/team-container/teamlist.ts
@@ -0,0 +1,25 @@
+const team = [
+    {
+        image: './igor.png',
+        name: 'Igor Bogdanov',
+        profession: 'Frontend Developer',
+        task: 'teamlead',
+        github: 'https://github.com/igorpex'
+    },
+    {
+        image: './vlad.png',
+        name: 'Vlad',
+        profession: 'Frontend Developer',
+        task: 'design',
+        github: 'https://github.com/Vladislav122-kor',
+    },
+    {
+        image: './sasha.png',
+        name: 'Aleksandra Pekhota',
+        profession: 'Frontend Developer',
+        task: 'sgfdfh',
+        github: 'https://github.com/takeAmoment'
+    }
+];
+
+export default team;
