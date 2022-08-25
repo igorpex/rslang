5fe9b92fa215818dbfdb26f9d634003da7566baf
alpekhota <alekspekhota@gmail.com>
2022-08-25 10:55:11 +0200

    feat: add methods addToLearned, addToDifficult, removeFromDifficult

--- src/components/book-container/bookItem.ts
+++ src/components/book-container/bookItem.ts
@@ -5,16 +5,22 @@ import UIButton from "../UI/button/button";
 import './bookContainer.scss'
 
 class BookItem extends Component{
-    deleteCard: (cardId: number) => void = () => {};
-    addToDifficult: (cardId: number) => void = () => {};
+    // addToLearned: (cardId: number) => void = () => {};
+    // addToDifficult: (cardId: number) => void = () => {};
 
     private card: Word;
-    private removeButton: UIButton;
+    private learnButton: UIButton;
     private addToDifficultButton: UIButton;
+    lernedWords: Word[];
+    difficultWords: Word[];
+    isDifficult: boolean;
 
     constructor(parentNode: HTMLElement, card: Word){
         super(parentNode, 'div', ['book-item']);
         this.card = card;
+        this.lernedWords  = [];
+        this.difficultWords = [];
+        this.isDifficult = false;
 
         const leftItem = new Component(this.element, 'div', ['book-item__left']);
         const rightItem = new Component(this.element, 'div', ['book-item__right']);
@@ -40,10 +46,44 @@ class BookItem extends Component{
         
         const textExampleTranslate = new Component(rightItem.element, 'p', ['item__text-example-translate'], `${this.card.textExampleTranslate}`);
         const buttonContainer = new Component(rightItem.element, 'div', ['item__buttons']);
-        this.removeButton = new UIButton(buttonContainer.element, ['item__remove-button'], 'Удалить');
+        this.learnButton = new UIButton(buttonContainer.element, ['item__remove-button'], 'Изучено');
         this.addToDifficultButton = new UIButton(buttonContainer.element, ['item__add-button'], 'Добавить в сложные')
     
-        const groupIcon = new Component(rightItem.element, 'span', ['item__icon', `group-${this.card.group + 1}`], `${this.card.group + 1}`)
+        const groupIcon = new Component(rightItem.element, 'span', ['item__icon', `group-${this.card.group + 1}`], `${this.card.group + 1}`);
+
+        this.learnButton.onClickButton = () => {
+            this.element.style.opacity = '0.6';
+            this.learnButton.setDisabled(true);
+            this.addToDifficultButton.setDisabled(true);
+            this.addToLearned(this.card);
+        };
+        this.addToDifficultButton.onClickButton = () => {
+            if(this.isDifficult === false) {
+                this.addToDifficultButton.element.innerHTML = 'Удалить из сложных'
+                this.addToDifficultButton.element.style.background = 'grey';
+                this.isDifficult = true;
+                this.addToDifficult(this.card, this.difficultWords);
+            } else if (this.isDifficult === true) {
+                this.addToDifficultButton.element.innerHTML = 'Добавить в сложные'
+                this.addToDifficultButton.element.style.background = 'rgba(86, 249, 214, 0.678)';
+                this.isDifficult = false;
+                this.removeFromDifficult(this.card);
+            }
+        };
+        
+    }
+    addToLearned(card: Word) {
+        this.lernedWords.push(card);
+        console.log(this.lernedWords);
+    }
+    addToDifficult(card: Word, arr: Word[]) {
+        arr.push(card);
+        console.log(arr);
+    }
+    removeFromDifficult(card: Word) {
+        const index = this.difficultWords.indexOf(card);
+        this.difficultWords.splice(index, 1);
+        console.log(this.difficultWords);
     }
 
     playAudio() {
