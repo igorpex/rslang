f6d8015c38cc9e3aa3557408466d2cb0cb53e89a
igorpex <igor.bogdanov@gmail.com>
2022-08-29 02:47:58 +0300

    feat: upadte mini-games page

--- src/components/games-container/games-container.ts
+++ src/components/games-container/games-container.ts
@@ -1,5 +1,4 @@
 import Component from '../../utils/component';
-
 import './index.scss';
 
 class GamesContainer extends Component {
@@ -11,18 +10,24 @@ class GamesContainer extends Component {
 
   private sprintBoxTitle: Component | undefined;
 
+  private gamesBox: Component;
+
+  private image: Component;
+
   constructor(parentNode: HTMLElement) {
     super(parentNode, 'div', ['games-container']);
-
+    this.gamesBox = new Component(this.element, 'div', ['games__box']);
     // this.gamesContainer = new GamesContainer(this.element);
-    this.audioChallengeBox = new Component(this.element, 'div', ['games__box', 'games__box_audio-challenge']);
-    this.sprintBox = new Component(this.element, 'div', ['games__box', 'games__box_sprint']);
+    this.audioChallengeBox = new Component(this.gamesBox.element, 'div', ['games__game', 'games__game_audio-challenge']);
+    this.sprintBox = new Component(this.gamesBox.element, 'div', ['games__game', 'games__game_sprint']);
 
-    this.audioChallengeBoxTitle = new Component(this.audioChallengeBox.element, 'h2', ['games__box-title']);
-    this.sprintBoxTitle = new Component(this.sprintBox.element, 'h2', ['games__box-title']);
+    this.audioChallengeBoxTitle = new Component(this.audioChallengeBox.element, 'h2', ['games__game-title']);
+    this.sprintBoxTitle = new Component(this.sprintBox.element, 'h2', ['games__game-title']);
 
-    this.audioChallengeBoxTitle.element.innerHTML = '<a href="#/games/audio-challenge">Аудиовызов</a>';
-    this.sprintBoxTitle.element.innerHTML = '<a href="#/games/sprint">Спринт</a>';
+    this.audioChallengeBoxTitle.element.innerHTML = '<a class="games__game-link" href="#/games/audio-challenge">Аудиовызов</a>';
+    this.sprintBoxTitle.element.innerHTML = '<a class="games__game-link" href="#/games/sprint">Спринт</a>';
+
+    this.image = new Component(this.element, 'div', ['games__image']);
   }
 
   private clear() {
