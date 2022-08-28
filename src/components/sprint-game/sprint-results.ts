e65fcfb8744f0eb04d2043502cc4f902408936f1
igorpex <igor.bogdanov@gmail.com>
2022-08-28 17:22:09 +0300

    feat: add empty template for future sprint game results

---
+++ src/components/sprint-game/sprint-results.ts
@@ -0,0 +1,10 @@
+import Component from '../../utils/component';
+
+import './index.scss';
+
+class SprintResults extends Component {
+  constructor(parentNode: HTMLElement) {
+    super(parentNode, 'div', ['sprint-results']);
+  }
+}
+export default SprintResults;
