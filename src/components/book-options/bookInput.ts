d1d76c10cace6f03ad402d65ab993ffe958699cc
alpekhota <alekspekhota@gmail.com>
2022-08-31 10:01:21 +0200

    feat: change select-form to level-list

--- src/components/book-options/bookInput.ts
+++ src/components/book-options/bookInput.ts
@@ -1,29 +1,48 @@
 import Component from "../../utils/component";
+import ListItem from "../shared/list/list";
 import UIInput from "../UI/input/input";
 
 class BookInput extends Component{
-    input: UIInput;
+    // input: UIInput;
     updateGroup: (group: number) => void =() => {};
     private group = 0;
     amountOfLevel = 7;
-
+    nameArray = ['Новичок', 'Ученик', 'Мыслитель',  'Кандидат',  'Мастер',  'Эксперт',
+          'Сложные'];
+    levelTitle: Component;
     constructor(
         parentNode: HTMLElement,
         buttonText: string,
         styles: string[] = [],
     ) {
-        super(parentNode, 'div', ['wrapper-input']);
-        this.input = new UIInput(this.element, 'select', ['book-input']);
-        const select = this.input.element as HTMLInputElement;
+        super(parentNode, 'div', ['wrapper-level']);
 
-        for( let i = 1; i <= this.amountOfLevel; i++) {
-          const option = new Component(select, 'option', ['input__option'], `Section ${i}`);
-          const item = option.element as HTMLInputElement;
-          item.value = `${i}`;
+        const levelHeader = new Component(this.element, 'div', ['level__header']);
+        const levelIcon = new Component(levelHeader.element, 'span', ['level__icon']);
+        levelIcon.element.style.backgroundImage = `url(./levels-icon.svg)`;
+        this.levelTitle = new Component(levelHeader.element, 'div', ['level__title']);
+        this.levelTitle.element.innerHTML = 'Новичок';
+        const levelBtn = new Component(levelHeader.element, 'span', ['level__btn']);
+        levelBtn.element.style.backgroundImage = `url(./down-arrow.svg)`;
+
+        const levelList = new Component(this.element, 'ul', ['level__list']);
+        for(let i = 0; i < this.nameArray.length; i++) {
+          const listItem = new ListItem(levelList.element, i, this.nameArray[i]);
+          listItem.onClickButton = (e) => {
+            const target = e.target as HTMLElement;
+            this.group = Number(target.getAttribute('data-group'));
+            this.redrawPage(target);
+          }
+          listItem.element.setAttribute('data-group', `${i}`);
         }
-        select.addEventListener('change', () => {
-            this.updateGroup(Number(select.value) - 1);
+        
+        this.element.addEventListener('click', () => {
+            levelList.element.classList.toggle('open');
           });
+    }
+    redrawPage(target: HTMLElement){
+      this.updateGroup(this.group);
+      this.levelTitle.element.innerHTML = `${target.innerHTML}`;
     }
 } 
 export default BookInput;
