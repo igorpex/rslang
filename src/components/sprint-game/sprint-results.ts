140563d10e7f871fd4478b511d6bd9e506236b15
igorpex <igor.bogdanov@gmail.com>
2022-09-03 17:14:12 +0300

    feat: add save game statistics (work in progress)

--- src/components/sprint-game/sprint-results.ts
+++ src/components/sprint-game/sprint-results.ts
@@ -1,6 +1,9 @@
 import Component from '../../utils/component';
 import './index.scss';
 import { ShortWord, SprintCounts } from '../../interfaces';
+import Auth from '../auth/auth/auth';
+import { baseUrl } from '../../api/api';
+import playBtn from '../../assets/svg/play.svg';
 
 class SprintResults extends Component {
   rightAnsweredWords: ShortWord[] | undefined;
@@ -15,9 +18,14 @@ class SprintResults extends Component {
 
   public start() {
     const rightContainer = new Component(this.element, 'div', ['sprint-results__answers']);
-    const rightContainerTitle = new Component(rightContainer.element, 'div', ['sprint-results__title'], 'Правильно отвеченные слова');
+    const rightContainerTitle = new Component(null, 'div', ['sprint-results__title'], 'Правильно отвеченные слова');
+    rightContainer.element.append(rightContainerTitle.element);
     this.rightAnsweredWords!.forEach((word: ShortWord) => {
       const wordBox = new Component(rightContainer.element, 'div', ['sprint__results-word-box']);
+      const wordAudio = new Audio(`${baseUrl}/${word.audio}`);
+      const playAudioBtn = new Component(wordBox.element, 'img', ['sprint__card-audio-btn', 'sprint__results-audio']);
+      playAudioBtn.element.setAttribute('src', playBtn);
+      playAudioBtn.element.addEventListener('click', () => wordAudio.play());
       const wordEn = new Component(wordBox.element, 'div', ['sprint__results-word'], word.word);
       const wordRu = new Component(wordBox.element, 'div', ['sprint__results-translate'], word.wordTranslate);
     });
@@ -26,9 +34,22 @@ class SprintResults extends Component {
     const wrongContainerTitle = new Component(wrongContainer.element, 'div', ['sprint-results__title'], 'Неправильно отвеченные слова');
     this.wrongAnsweredWords!.forEach((word: ShortWord) => {
       const wordBox = new Component(wrongContainer.element, 'div', ['sprint__results-word-box']);
+      const wordAudio = new Component(wordBox.element, 'audio', ['sprint__results-audio'], word.audio);
       const wordEn = new Component(wordBox.element, 'div', ['sprint__results-word'], word.word);
       const wordRu = new Component(wordBox.element, 'div', ['sprint__results-translate'], word.wordTranslate);
     });
+
+    this.saveStatistics();
+  }
+
+  async saveStatistics() {
+    const sprintResults = {
+      maxRightInTheRow: this.sprintCounts?.maxRightInTheRow,
+      rightAnswers: this.rightAnsweredWords?.length,
+      wrongAnswers: this.wrongAnsweredWords?.length,
+      date: new Date(),
+    };
+    console.log('sprintResults:', sprintResults);
   }
 }
 export default SprintResults;
