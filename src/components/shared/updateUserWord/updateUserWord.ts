9543f42a95249ef0b6fae4f229519280aae5dd69
igorpex <igor.bogdanov@gmail.com>
2022-09-03 16:48:18 +0300

    feat: add function updateUserWord statistics
    it checks if word exist or not and create or update it

--- src/components/shared/updateUserWord/updateUserWord.ts
+++ src/components/shared/updateUserWord/updateUserWord.ts
@@ -1,101 +1,83 @@
-import { SprintWord } from '../../../interfaces';
+import {
+  Game, SprintWord, UserWord, Word,
+} from '../../../interfaces';
 import Auth from '../../auth/auth/auth';
 import { authStorageKey } from '../../../utils/config';
-import { createUserWord, getUserWordById, updateUserWord } from '../../../api/api';
+import {
+  createUserWord, getUserWordByIdWithStatus, updateUserWord,
+} from '../../../api/api';
+import { createEmptyUserWord } from '../emptyUserWord/emptyUserWord';
 
-async function updateWordStatistics(game: 'sprint' | 'audioChallenge', result: 'right' | 'wrong', word: SprintWord) {
+function updateUserWordObject(game: Game, result: 'right' | 'wrong', userWord: UserWord) {
+  const updatedUserWord = userWord;
+  // Create New status if game is not new;
+  if (!updatedUserWord.optional.dateNew) {
+    updatedUserWord.optional.dateNew = Date.now();
+    updatedUserWord.optional.gameNew = game;
+  }
+
+  if (result === 'right') {
+    // Update game success counter
+    updatedUserWord.optional[game].successCounter += 1;
+
+    // in case of correct answer - update it on the way to become learned(rightInARowToEasy)
+    updatedUserWord.optional.rightInARow += 1;
+
+    // Update learned status and date
+    const { difficulty } = updatedUserWord;
+    switch (difficulty) {
+      case 'hard':
+        if (updatedUserWord.optional.rightInARow >= 5) {
+          updatedUserWord.difficulty = 'easy';
+          updatedUserWord.optional.dateEasy = Date.now();
+        }
+        break;
+      case 'normal' || null:
+        if (updatedUserWord.optional.rightInARow >= 3) {
+          updatedUserWord.difficulty = 'easy';
+          updatedUserWord.optional.dateEasy = Date.now();
+        }
+        break;
+      default:
+        break;
+    }
+  } else if (result === 'wrong') {
+    // Update game fail counter
+    updatedUserWord.optional[game].failCounter += 1;
+
+    // reset in a Row counter)
+    updatedUserWord.optional.rightInARow = 0;
+
+    // Make it hard
+    updatedUserWord.difficulty = 'hard';
+
+    // Reset Learned date to 0, because it is not learned anymore
+    updatedUserWord.optional.dateEasy = 0;
+  }
+
+  return updatedUserWord;
+  // case if user word does not exist - А new userWord and POST to server
+}
+
+async function updateWordStatistics(game: Game, result: 'right' | 'wrong', word: SprintWord | Word) {
   const auth = new Auth();
   if (!auth.JwtHasExpired()) {
     const userAuthData = localStorage.getItem(authStorageKey);
     const { userId, token } = JSON.parse(userAuthData!);
     // case if user word does exist - when get curren user word info, update it and PUT to server
-    try {
-      const userWord = await getUserWordById(userId, word.id, token);
-      const updatedUserWord = userWord;
-      const { difficulty } = userWord;
-      if (result === 'right') {
-        // in case of correct answer - update it on the way to become learned(rightInARowToEasy)
-        // eslint-disable-next-line max-len
-        updatedUserWord.optional.gameStat[game].successCounter = updatedUserWord.optional.gameStat[game].successCounter ? updatedUserWord.optional.gameStat[game].successCounter + 1 : 1;
-        switch (difficulty) {
-          case 'hard':
-            // eslint-disable-next-line max-len
-            if (updatedUserWord.optional.easy && updatedUserWord.optional.easy.rightInARowToEasy >= 5) {
-              updatedUserWord.difficulty = 'easy';
-              updatedUserWord.optional.easy.rightInARowToEasy += 1;
-            }
-            break;
-          case 'normal' || null:
-            // eslint-disable-next-line max-len
-            if (updatedUserWord.optional.easy && updatedUserWord.optional.easy.rightInARowToEasy >= 3) {
-              updatedUserWord.difficulty = 'easy';
-              updatedUserWord.optional.easy.rightInARowToEasy += 1;
-            }
-            break;
-          case 'easy':
-            // eslint-disable-next-line max-len
-            if (updatedUserWord.optional.easy && updatedUserWord.optional.easy.rightInARowToEasy) {
-              updatedUserWord.optional.easy.rightInARowToEasy += 1;
-            }
-            break;
-          default:
-            break;
-        }
-      } else {
-        // in case of wrong answer make it hard and reset all the way for the word to become learned (easy)
-        updatedUserWord.difficulty = 'hard';
-        updatedUserWord.optional.easy = null;
-        // eslint-disable-next-line max-len
-        updatedUserWord.optional.gameStat[game].failCounter = updatedUserWord.optional.gameStat[game].failCounter ? updatedUserWord.optional.gameStat[game].failCounter + 1 : 1;
-      }
-      const updateUserWordResult = await updateUserWord(userId, word.id, token, updatedUserWord);
-      console.log('updateUserWordResult:', updateUserWordResult);
-    } catch { // case if user word does not exist - create new userWord and POST to server
+    const userWordWStatus = await getUserWordByIdWithStatus(userId, word.id, token);
+
+    if (userWordWStatus.status === 404) {
       console.log('word is new, not in userWords');
-      let userWord;
-      if (result === 'right') {
-        userWord = {
-          difficulty: 'normal',
-          optional: {
-            new: {
-              isNew: true,
-              dateNew: Date.now(),
-              gameNew: game,
-            },
-            easy: {
-              dateEasy: null,
-              rightInARowToEasy: 1,
-            },
-            gamesStat: {
-              sprint: { successCounter: 0, failCounter: 0 },
-              audioChallenge: { successCounter: 0, failCounter: 0 },
-            },
-          },
-        };
-        userWord.optional.gamesStat[game] = { successCounter: 1, failCounter: 0 };
-      } else {
-        userWord = {
-          difficulty: 'hard',
-          optional: {
-            new: {
-              isNew: true,
-              dateNew: Date.now(),
-              gameNew: game,
-            },
-            easy: {
-              dateEasy: null,
-              rightInARowToEasy: 0,
-            },
-            gamesStat: {
-              sprint: { successCounter: 0, failCounter: 0 },
-              audioChallenge: { successCounter: 0, failCounter: 0 },
-            },
-          },
-        };
-        userWord.optional.gamesStat[game] = { successCounter: 1, failCounter: 0 };
-      }
-      const createUserWordResult = await createUserWord(userId, word.id, token, userWord);
-      console.log('createUserResult:', createUserWordResult);
+      const newUserWord = createEmptyUserWord();
+      const updatedUserWord = updateUserWordObject(game, result, newUserWord);
+      await createUserWord(userId, word.id, token, updatedUserWord);
+    } else if (userWordWStatus.status === 200) {
+      const userWord = userWordWStatus.data;
+      const updatedUserWord = updateUserWordObject(game, result, userWord);
+      await updateUserWord(userId, word.id, token, updatedUserWord);
+    } else {
+      console.log('Error requesting user word');
     }
   }
 }
