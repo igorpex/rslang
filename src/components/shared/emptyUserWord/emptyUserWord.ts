fbc273560a2cf4d0f2b0d3624a4bd87cc98b5b86
igorpex <igor.bogdanov@gmail.com>
2022-09-03 16:47:15 +0300

    feat: add EmptyUserWord function to get similar userWord objects in different places

---
+++ src/components/shared/emptyUserWord/emptyUserWord.ts
@@ -0,0 +1,30 @@
+import { Difficulty, UserWord, WordOptional } from '../../../interfaces';
+
+export class UserWordClass implements UserWord {
+  public difficulty: Difficulty;
+
+  public optional: WordOptional;
+
+  constructor() {
+    this.difficulty = 'normal';
+    this.optional = {
+      dateNew: 0,
+      gameNew: 'sprint',
+      dateEasy: 0,
+      rightInARow: 0,
+      sprint: {
+        successCounter: 0,
+        failureCounter: 0,
+      },
+      audioChallenge: {
+        successCounter: 0,
+        failureCounter: 0,
+      },
+    };
+  }
+}
+
+export function createEmptyUserWord() {
+  const userWord = new UserWordClass();
+  return userWord;
+}
