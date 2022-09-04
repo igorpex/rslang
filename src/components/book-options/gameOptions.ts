eca21efaa851ab8d35a2c7b704d3ecc12182ec1c
alpekhota <alekspekhota@gmail.com>
2022-09-04 16:48:17 +0200

    feat: add href for audioChallenge link

--- src/components/book-options/gameOptions.ts
+++ src/components/book-options/gameOptions.ts
@@ -23,27 +23,22 @@ class GameOptions extends Component {
 
     const sprintBtn = new Component(gameList.element, 'li', ['list__item']);
     const sprintLink = new Component(sprintBtn.element, 'a', ['sprint__link'], 'Спринт');
-    sprintLink.element.setAttribute('href', '#/games/sprint');
+    sprintLink.element.setAttribute('href', '?ref=%23%2Febook#/games/sprint');
     const audioGameBtn = new Component(gameList.element, 'li', ['list__item'], '');
     const audioGameLink = new Component(audioGameBtn.element, 'a', ['audio-game__link'], 'Аудиовызов');
-    audioGameLink.element.setAttribute('href', '#/games/audio-challenge');
+    audioGameLink.element.setAttribute('href', '?ref=%23%2Febook#/games/audio-challenge');
 
     this.element.addEventListener('click', () => {
       gameList.element.classList.toggle('hidden');
     });
     sprintBtn.element.addEventListener('click', () => {
-      gameList.element.classList.remove('hidden');
-      localStorage.setItem ("previourUrl", window.location.href);
     });
     audioGameBtn.element.addEventListener('click', () => {
-      gameList.element.classList.remove('hidden');
-      localStorage.setItem ("previourUrl", window.location.href);
     });
     // sprintBtn.onClickButton = () => {
     //     this.startSprintGame();
     //     console.log('start');
     // }
-    // audioGameBtn.onClickButton = () => this.startAudioGame();
     // dictionaryBtn.onClickButton = () => this.openDictionary();
   }
 }
