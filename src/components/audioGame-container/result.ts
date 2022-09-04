a5d13d0235ba109a269b1eb4e7657c5c05ee3334
alpekhota <alekspekhota@gmail.com>
2022-09-04 12:27:29 +0200

    feat: add checkPreviousPage to result

--- src/components/audioGame-container/result.ts
+++ src/components/audioGame-container/result.ts
@@ -8,6 +8,7 @@ class Result extends Component{
     trueList: Component;
     result: StatisticsObject[];
     falseList: Component;
+    buttonsContainer: Component;
 
     constructor(parentNode: HTMLElement, result: StatisticsObject[]){
         super(parentNode, 'div', ['audioChallenge-result__container']);
@@ -21,12 +22,58 @@ class Result extends Component{
         const falseTitle = new Component(resultContent.element, 'p', ['false-title'], 'Неправильных ответов:');
         this.falseList = new Component(resultContent.element, 'ul', ['false-list']);
 
-        const buttonsContainer = new Component(this.element, 'div', ['audioChallenge-result__buttons']);
-        const repeatButton = new UIButton(buttonsContainer.element, ['repeat-button'], 'Играть еще');
-        const returnButton = new UIButton(buttonsContainer.element, ['return-button'], 'К списку игр');
+        this.buttonsContainer = new Component(this.element, 'div', ['audioChallenge-result__buttons']);
+        const repeatButton = new Component(this.buttonsContainer.element, 'a', ['audioChallenge-repeat__button'], 'Играть еще');
+        repeatButton.element.setAttribute('href', '#/games/audioChallenge');
+        repeatButton.element.addEventListener('click', (e) => {
+          e.preventDefault();
+          window.location.reload();
+        });
+        // const burger = document.querySelector('.header__burger');
+        // burger?.addEventListener('click', () => {
+        //     const params = new URLSearchParams(document.location.search);
+        //     const ref = params.get('ref');
+        //     let next = '';
+        //     if (ref) {
+        //         next = ref.slice(1);
+        //     }
+        //     const loc = window.location;
+        //     loc.hash = next;
+        //     const url = new URL(loc.href);
+        //     url.searchParams.delete('ref');
+        //     window.location.replace(url);
+        // })
+
+        this.checkPreviousPage();
 
         this.filterTrueAnswers();
         this.filterFalseAnswers();
+    }
+    checkPreviousPage(){
+        const params = new URLSearchParams(document.location.search);
+        const ref = params.get('ref');
+        if(ref !== null) {
+            console.log(ref);
+            if(ref!.includes('ebook')) {
+                const buttonsListGames = new Component(this.buttonsContainer.element, 'a', ['audioChallenge-return__button'], 'К СЛОВАРЮ');
+                buttonsListGames.element.setAttribute('href', `#`);
+                buttonsListGames.element.addEventListener('click', (e) => {
+                e.preventDefault();
+                let next = '';
+                if (ref) {
+                    next = ref.slice(1);
+                }
+                const loc = window.location;
+                loc.hash = next;
+                const url = new URL(loc.href);
+                url.searchParams.delete('ref');
+                window.location.replace(url);
+                });
+            } 
+        } else {
+            const buttonsListGames = new Component(this.buttonsContainer.element, 'a', ['audioChallenge-return__button'], 'К СПИСКУ ИГР');
+            buttonsListGames.element.setAttribute('href', '#/games');
+        }
     }
 
     filterTrueAnswers() {
